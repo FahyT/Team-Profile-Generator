20ad58c9a20531d43b787b399909294a3d0a4205
Tom Fahy <thomas.fahy9@gmail.com>
2024-01-14 19:51:05 +0000

    add questions for inquirer to index.js

--- index.js
+++ index.js
@@ -13,3 +13,47 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+
+
+const generateTeamMember = (teamMember) => {
+    inquirer
+    .prompt([{
+        type: 'input',
+        message: `Enter the ${teamMember}'s name: `,
+        name: 'memberName'
+    },
+    {
+        type: 'input',
+        message: `Enter the ${teamMember}'s Employee ID: `,
+        name: 'memberID'
+    },
+    {
+        type: 'input',
+        message: `Enter the ${teamMember}'s email address: `,
+        name: 'memberEmail'
+    },
+    {
+        type: 'input',
+        message: `Enter the ${teamMember}'s Office number : `,
+        name: 'tmOfficeNumber',
+        when: teamMember == "Manager"
+    },
+    {
+        type: 'input',
+        message: `Enter the ${teamMember}'s GitHub username: `,
+        name: 'tmGitHub',
+        when: teamMember == "Engineer"
+    },
+    {
+        type: 'input',
+        message: `Enter the ${teamMember}'s School: `,
+        name: 'tmSchool',
+        when: teamMember == "Intern"
+    },
+    ])
+    .then((answers) => {
+        console.log(answers);
+      })
+}
+
+generateTeamMember("Manager");
